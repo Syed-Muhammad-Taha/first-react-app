02399ef20b01114e968c2ac7a9ee60fc23ee959b
Taha <chaoticcriminal123@gmail.com>
2023-01-20 23:02:58 +0500

    my-first-commit

---
+++ src/movieCard.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const movieCard = ({m1}) => {
+  return (
+    <div className="movie">
+      <div>
+        <p>{m1.Year}</p>
+      </div>
+      <div>
+        <img
+          src={
+            m1.Poster !== "N/A" ? m1.Poster : "https://via.placeholder.com/400"
+          }
+          alt={m1.Title}
+        />
+      </div>
+      <div>
+        <span>{m1.Type}</span>
+        <h3>{m1.Title}</h3>
+      </div>
+    </div>
+  );
+};
+
+export default movieCard;
